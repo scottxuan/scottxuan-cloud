7eab1e7933c347f0d310ec88f12129421449a413
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-06-17 10:29:36 +0800

    swagger

---
+++ scottxuan-cloud-web/src/main/java/scottxuan/cloud/web/swagger/AbstractSwaggerConfig.java
@@ -0,0 +1,39 @@
+package scottxuan.cloud.web.swagger;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public abstract class AbstractSwaggerConfig {
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(getBasePackage()))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(getTitle())
+                .description(getDescription())
+                .version(getVersion())
+                .build();
+    }
+
+    abstract String getBasePackage();
+
+    abstract String getTitle();
+
+    abstract String getDescription();
+
+    abstract String getVersion();
+}
