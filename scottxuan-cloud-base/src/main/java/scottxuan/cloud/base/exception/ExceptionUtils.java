646c9fcc4e16d992df7121c02d513899621104e6
scottxuan <zhaoxuan734@163.com>
2020-06-18 23:31:09 +0800

    pom repair

--- scottxuan-cloud-base/src/main/java/scottxuan/cloud/base/exception/ExceptionUtils.java
+++ scottxuan-cloud-base/src/main/java/scottxuan/cloud/base/exception/ExceptionUtils.java
@@ -1,16 +1,18 @@
 package scottxuan.cloud.base.exception;
 
 
+import scottxuan.cloud.base.error.IError;
+
 /**
  * @author : zhaoxuan
  */
 public class ExceptionUtils {
 
-    public static void throwException(Error error) {
+    public static void throwException(IError error) {
         throw new BizException(error);
     }
 
-    public static void throwException(Error error, Object... args) {
+    public static void throwException(IError error, Object... args) {
         if (args.length > 0) {
             throw new BizException(error, args);
         } else {
