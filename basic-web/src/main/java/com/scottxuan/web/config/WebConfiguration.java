bbd8adc6445f3e96606399c549c0ff2c9660b788
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-15 15:52:14 +0800

    INIT

---
+++ basic-web/src/main/java/com/scottxuan/web/config/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.scottxuan.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : scottxuan
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    /**
+     * 访问静态资源
+     * */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
